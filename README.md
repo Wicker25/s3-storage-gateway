911e742e42b64501ce8744721411db31f59c20c4
Wicker25 <wicker25@gmail.com>
2020-01-29 18:19:18 +0000

    Added `etcd` container

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ TODO
 ## Getting started
 
 ```
+$ docker-compose build
+
 $ AWS_ACCESS_KEY_ID='xxxxxx' \
   AWS_SECRET_ACCESS_KEY='xxxxxx' \
   AWS_ENDPOINT_URL='s3.us-east-2.amazonaws.com' \
--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
+# Generate the configuration files
 envsubst '${AWS_ENDPOINT_URL}' < /etc/nginx/conf.d/default.conf.in > /etc/nginx/conf.d/default.conf
 
 nginx -g 'daemon off;'
